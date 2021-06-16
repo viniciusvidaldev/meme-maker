b78895b53a0f816c06fecbf779ef6e431ce17266
Vinicius Vidal <vidalvinicius007@gmail.com>
2021-06-16 19:45:11 -0300

    project done

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import qs from 'qs';
 
 import { Wrapper, Card, Templates, Form, Button } from './styles';
 import logo from '../../images/logo.svg';
@@ -21,6 +22,44 @@ const Home: React.FC = () => {
   const [templates, setTemplates] = useState<ITemplates[]>([]);
   const [selectedTemplate, setSelectedTemplate] =
     useState<ISelectedTemplates | null>(null);
+  const [boxes, setBoxes] = useState<string[]>([]);
+  const [generatedMeme, setGeneratedMeme] = useState<string | null>(null);
+
+  const handleInputChange =
+    (index: number) => (e: ChangeEvent<HTMLInputElement>) => {
+      const newValues = boxes;
+      newValues[index] = e.target.value;
+      setBoxes(newValues);
+    };
+
+  function handleSelectedTemplate(template: ISelectedTemplates) {
+    setSelectedTemplate(template);
+    setBoxes([]);
+  }
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const params = qs.stringify({
+      template_id: selectedTemplate?.id,
+      username: 'vikayel543',
+      password: 'vikayel543',
+      boxes: boxes.map((text) => ({ text })),
+    });
+
+    const resp = await fetch(`https://api.imgflip.com/caption_image?${params}`);
+    const {
+      data: { url },
+    } = await resp.json();
+
+    setGeneratedMeme(url);
+  }
+
+  function handleReset() {
+    setSelectedTemplate(null);
+    setBoxes([]);
+    setGeneratedMeme(null);
+  }
 
   useEffect(() => {
     (async () => {
@@ -37,14 +76,22 @@ const Home: React.FC = () => {
       <img src={logo} alt='MemeMaker' />
 
       <Card>
-        <h2>Selecione um template</h2>
+        {generatedMeme && (
+          <>
+            <img src={generatedMeme} alt='generated meme' />
+            <Button type="button" onClick={handleReset}>Create another meme</Button>
+          </>
+        )}
+        {!generatedMeme && (
+          <>
+            <h2>Selecione um template</h2>
         <Templates>
           {templates.map((template) => (
             <button
               key={template.id}
               type='button'
               onClick={() => {
-                setSelectedTemplate(template);
+                handleSelectedTemplate(template);
               }}
               className={template.id === selectedTemplate?.id ? 'selected' : ''}
             >
@@ -56,14 +103,21 @@ const Home: React.FC = () => {
         {selectedTemplate && (
           <>
             <h2>Textos</h2>
-            <Form>
-              {(new Array(selectedTemplate.box_count)).fill('').map((_, index) => (
-                  <input key={String(Math.random())} placeholder={`Text #${index + 1}`} />
-                ))
-              }
+            <Form onSubmit={handleSubmit}>
+              {new Array(selectedTemplate.box_count)
+                .fill('')
+                .map((_, index) => (
+                  <input
+                    key={String(Math.random())}
+                    placeholder={`Text #${index + 1}`}
+                    onChange={handleInputChange(index)}
+                  />
+                ))}
 
-              <Button>MakeMyMeme!</Button>
+              <Button type='submit'>MakeMyMeme!</Button>
             </Form>
+          </>
+        )}
           </>
         )}
       </Card>
